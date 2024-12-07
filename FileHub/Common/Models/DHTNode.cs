f700a66135bd43076ee7df0079496c4bdc978212
fiZZowsky <piotrekr53@interia.pl>
2024-12-07 22:36:46 +0100

    usunięto wszystko

--- FileHub/Common/Models/DHTNode.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Common.Models
-{
-    public class DHTNode
-    {
-        public string Address { get; set; }
-        public int Port { get; set; }
-        public int Hash { get; set; }
-    }
-}
